b1dddc46e5b13d6bd5fe45f7888af419e5785a40
mywnajsldkf <mywnajsldkf@gmail.com>
2023-07-12 10:33:56 +0900

    Feat : name과 position을 반환하는 함수 추가
    Co-authored-by: mywnajsldkf <mywnajsldkf@gmail.com>
    Co-authored-by: wnstn819 <wnstn819@naver.com>
    Co-authored-by: chwangmin <ckm7907cb@gmail.com>

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -29,5 +29,11 @@ public class Car {
         }
     }
 
-    // 추가 기능 구현
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
